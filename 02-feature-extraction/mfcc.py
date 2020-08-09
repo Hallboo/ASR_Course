083f0b28830c5c8bede08b2d33ff725e7e180f56
Ubuntu <zhb502@MySSH.mewh1t20ihnehng0cyxfpntk5a.ix.internal.cloudapp.net>
2020-08-09 14:31:45 +0000

    2020-08-09

--- 02-feature-extraction/mfcc.py
+++ 02-feature-extraction/mfcc.py
@@ -1,32 +1,48 @@
 import librosa
+import librosa.display
 import numpy as np
 from scipy.fftpack import dct
 
 # If you want to see the spectrogram picture
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-# def plot_spectrogram(spec, note,file_name):
-#     """Draw the spectrogram picture
-#         :param spec: a feature_dim by num_frames array(real)
-#         :param note: title of the picture
-#         :param file_name: name of the file
-#     """ 
-#     fig = plt.figure(figsize=(20, 5))
-#     heatmap = plt.pcolor(spec)
-#     fig.colorbar(mappable=heatmap)
-#     plt.xlabel('Time(s)')
-#     plt.ylabel(note)
-#     plt.tight_layout()
-#     plt.savefig(file_name)
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
-#preemphasis config 
+def plot_spectrogram(spec, note, filepath):
+    """Draw the spectrogram picture
+        :param spec: a feature_dim by num_frames array(real)
+        :param note: title of the picture
+        :param file_name: name of the file
+    """
+    fig = plt.figure(figsize=(20, 5))
+    heatmap = plt.pcolor(spec)
+    fig.colorbar(mappable=heatmap)
+    plt.xlabel('Time(s)')
+    plt.ylabel(note)
+    plt.tight_layout()
+    fig.savefig(filepath, format='png')
+
+
+def PlotSpecFromWav(y, note):
+    D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+
+    fig = plt.figure(figsize=(20, 5))
+    heatmap = plt.pcolor(D)
+    fig.colorbar(mappable=heatmap)
+    librosa.display.specshow(D, y_axis='log')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Log-frequency power spectrogram %s' % note)
+    plt.show()
+
+
+# preemphasis config
 alpha = 0.97
 
 # Enframe config
-frame_len = 400      # 25ms, fs=16kHz
-frame_shift = 160    # 10ms, fs=15kHz
+frame_len = 400  # 25ms, fs=16kHz
+frame_shift = 160  # 10ms, fs=15kHz
 fft_len = 512
 
 # Mel filter config
@@ -35,6 +51,7 @@ num_mfcc = 12
 
 # Read wav file
 wav, fs = librosa.load('./test.wav', sr=None)
+
 
 # Enframe with Hamming window function
 def preemphasis(signal, coeff=alpha):
@@ -46,6 +63,7 @@ def preemphasis(signal, coeff=alpha):
     """
     return np.append(signal[0], signal[1:] - coeff * signal[:-1])
 
+
 def enframe(signal, frame_len=frame_len, frame_shift=frame_shift, win=np.hamming(frame_len)):
     """Enframe with Hamming widow function.
 
@@ -53,15 +71,16 @@ def enframe(signal, frame_len=frame_len, frame_shift=frame_shift, win=np.hamming
         :param win: window function, default Hamming
         :returns: the enframed signal, num_frames by frame_len array
     """
-    
+
     num_samples = signal.size
-    num_frames = np.floor((num_samples - frame_len) / frame_shift)+1
-    frames = np.zeros((int(num_frames),frame_len))
+    num_frames = int(np.floor((num_samples - frame_len) / frame_shift) + 1)
+    frames = np.zeros((num_frames, frame_len))
     for i in range(int(num_frames)):
-        frames[i,:] = signal[i*frame_shift:i*frame_shift + frame_len] 
-        frames[i,:] = frames[i,:] * win
+        frames[i, :] = signal[i * frame_shift:i * frame_shift + frame_len]
+        frames[i, :] = frames[i, :] * win
 
     return frames
+
 
 def get_spectrum(frames, fft_len=fft_len):
     """Get spectrum using fft
@@ -70,48 +89,115 @@ def get_spectrum(frames, fft_len=fft_len):
         :returns: spectrum, a num_frames by fft_len/2+1 array (real)
     """
     cFFT = np.fft.fft(frames, n=fft_len)
-    valid_len = int(fft_len / 2 ) + 1
-    spectrum = np.abs(cFFT[:,0:valid_len])
+    valid_len = int(fft_len / 2) + 1
+    spectrum = np.abs(cFFT[:, 0:valid_len])
     return spectrum
 
-def fbank(spectrum, num_filter = num_filter):
+
+def DefineFilterOnMelScale(num_filter, fmax=8000):
+    fmax_mel = Freq2Mel(fmax)
+
+    interval = fmax_mel / (num_filter + 1)
+    filter_def = np.zeros((num_filter, 2))
+    # (num_filter, [freq_start, freq_end])
+
+    for i in range(filter_def.shape[0]):
+        filter_def[i][0] = interval * i
+        filter_def[i][1] = interval * (i + 2)
+
+    return filter_def
+
+
+def Mel2Freq(value_mel):
+    value_freq = (10 ** (value_mel / 2595) - 1) * 700
+    return value_freq
+
+
+def Freq2Mel(value_freq):
+    return 2595 * np.log10(1 + value_freq / 700)
+
+
+def MelScale2FreqScale(filter_def_mel):
+    filter_def_freq = Mel2Freq(filter_def_mel)
+    return filter_def_freq
+
+
+def BuildMelFilters(filter_def, fft_len, fmax = 8000):
+    len = int(fft_len / 2 + 1)
+    filters = np.zeros((filter_def.shape[0], len))
+    for i in range(filters.shape[0]):
+        filters[i] = GetOneFilter(
+            filter_def[i][0], filter_def[i][1], fft_len, fmax)
+    return filters
+
+
+def GetOneFilter(start, end, fft_len, fmax):
+    from scipy import signal
+    len = int(fft_len / 2 + 1)
+    num = int(((end - start) / fmax) * len)
+    start_point = int((start / fmax) * len)
+    end_point = start_point + num
+
+    filter = np.zeros((len))
+    filter[start_point:end_point] = signal.triang(num)
+
+    return filter
+
+
+def GetMelFilters(fft_len, num_filter):
+    filter_def = DefineFilterOnMelScale(num_filter)
+    filter_def = MelScale2FreqScale(filter_def)
+    filter_mel = BuildMelFilters(filter_def, fft_len)
+    return filter_mel
+
+
+def PlotMelFilters(M, note, filepath):
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    librosa.display.specshow(M, x_axis='linear')
+    plt.ylabel('Mel filter')
+    plt.title('Mel filter bank %s' % note)
+    plt.colorbar()
+    plt.tight_layout()
+    fig.savefig(filepath, format='png')
+
+
+def fbank(spectrum, num_filter=num_filter, norm=None):
     """Get mel filter bank feature from spectrum
         :param spectrum: a num_frames by fft_len/2+1 array(real)
         :param num_filter: mel filters number, default 23
         :returns: fbank feature, a num_frames by num_filter array 
         DON'T FORGET LOG OPRETION AFTER MEL FILTER!
     """
-
-    feats=np.zeros(spectrum.shape[0], num_filter))
-    """
-        FINISH by YOURSELF
-    """
+    filters = GetMelFilters(fft_len, num_filter)
+    feats = np.log(np.dot(spectrum, filters.T))
+    PlotMelFilters(filters, 'MyFilters', 'filters.png')
     return feats
 
-def mfcc(fbank, num_mfcc = num_mfcc):
+
+def mfcc(fbank, num_mfcc=num_mfcc):
     """Get mfcc feature from fbank feature
         :param fbank: a num_frames by  num_filter array(real)
         :param num_mfcc: mfcc number, default 12
         :returns: mfcc feature, a num_frames by num_mfcc array 
     """
 
-    feats = np.zeros((fbank.shape[0],num_mfcc))
-    """
-        FINISH by YOURSELF
-    """
+    feats = dct(fbank, type=2, axis=1, norm='ortho')[:, 1: (num_mfcc + 1)]
+
     return feats
+
 
 def write_file(feats, file_name):
     """Write the feature to file
         :param feats: a num_frames by feature_dim array(real)
         :param file_name: name of the file
     """
-    f=open(file_name,'w')
-    (row,col) = feats.shape
+    f = open(file_name, 'w')
+    (row, col) = feats.shape
     for i in range(row):
         f.write('[')
         for j in range(col):
-            f.write(str(feats[i,j])+' ')
+            f.write(str(feats[i, j]) + ' ')
         f.write(']\n')
     f.close()
 
@@ -123,10 +209,11 @@ def main():
     spectrum = get_spectrum(frames)
     fbank_feats = fbank(spectrum)
     mfcc_feats = mfcc(fbank_feats)
-    # plot_spectrogram(fbank_feats, 'Filter Bank','fbank.png')
-    write_file(fbank_feats,'./test.fbank')
-    # plot_spectrogram(mfcc_feats.T, 'MFCC','mfcc.png')
-    write_file(mfcc_feats,'./test.mfcc')
+    plot_spectrogram(fbank_feats.T, 'Filter Bank', 'fbank.png')
+    write_file(fbank_feats, './test.fbank')
+    plot_spectrogram(mfcc_feats.T, 'MFCC', 'mfcc.png')
+    write_file(mfcc_feats, './test.mfcc')
+
 
 if __name__ == '__main__':
     main()
--- 03-GMM-EM/my_gmm.py
+++ 03-GMM-EM/my_gmm.py
@@ -1,17 +1,155 @@
+# Author: Sining Sun , Zhanheng Yang
+# Modified by Hallboo
 
+import sys
 import numpy as np
 from utils import *
 import scipy.cluster.vq as vq
 
 num_gaussian = 5
-num_uterations = 5
+num_iterations = 5
 targets = ['Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
 
+class GMM(object):
+    def __init__(self, D, K=5, data_for_init = 'train/feats.scp'):
+        assert(D>0)
+        self.dim = D
+        self.K   = K
+
+        self.mu, self.sigma, self.pi = self.kmeans_initial(data_for_init)
+
+    def kmeans_initial(self, data_path):
+        mu    = []
+        sigma = []
+        data  = read_all_data(data_path)
+        centroids, labels = vq.kmeans2(data, self.K, minit="points", iter=100)
+
+        clusters = [[] for i in range(self.K)]
+        for (l,d) in zip(labels, data):
+            clusters[l].append(d)
+        for cluster in clusters:
+            mu.append(np.mean(cluster, axis=0))
+            sigma.append(np.cov(cluster, rowvar=0))
+        mu = np.array(mu)
+        sigma = np.array(sigma)
+        pi = np.array([float(len(c)) / len(data) for c in clusters])
+
+        return mu, sigma, pi
+
+    def gaussian(self, x, mu, sigma, eps = 1e-4):
+        D = x.shape[0]
+        det_sigma = np.linalg.det(sigma)
+        inv_sigma = np.linalg.inv(sigma + eps)
+        mahalanobis = np.dot(np.transpose(x-mu), inv_sigma)
+        mahalanobis = np.dot(mahalanobis, (x-mu))
+        const_a = 1/((2*np.pi)**(D/2))
+        const_b = (det_sigma)**(-0.5)
+        return const_a * const_b * np.exp(-0.5 * mahalanobis)
+
+    def calc_log_likelihood(self, X):
+        log_llh = 0.0
+
+        for n in range(X.shape[0]):
+            p = 0.0
+            for k in range(self.K):
+                p += self.pi[k] * self.gaussian(X[n], self.mu[k], self.sigma[k])
+            log_llh += np.log(p)
+        return log_llh
+
+    def calc_gama_Znk(self, X):
+        gama_Znk = []
+        for n in range(X.shape[0]):
+            ps = []
+            for k in range(self.K):
+                ps.append(self.pi[k] * self.gaussian(
+                    X[n], self.mu[k], self.sigma[k]))
+            ps = np.array(ps)
+            gama_Znk.append(ps/np.sum(ps))
+        return np.array(gama_Znk)
+
+    def update_sigma(self, X, gama_Znk, Nk, mu_new):
+        temp = []
+        for k in range(self.K):
+            sums = np.zeros((self.dim, self.dim))
+            for n in range(X.shape[0]):
+                diff = X[n] - mu_new[k]
+                diff = diff.reshape(self.dim, 1)
+                sums+= gama_Znk[n][k] * np.dot(diff, diff.T)
+            temp.append(sums)
+        Nk = np.array([t*np.ones((self.dim,self.dim)) for t in Nk])
+        return np.array(temp)/Nk
+
+    def update_mu(self, X, gama_Znk, Nk):
+        mu_new = np.zeros((self.K, self.dim))
+        sums = np.zeros((self.K, self.dim))
+        for n in range(X.shape[0]):
+            sums+=np.dot(gama_Znk[n].reshape(self.K,1), X[n].reshape(1,self.dim))
+        return sums/(np.dot(Nk.reshape(self.K,1), np.ones((1,self.dim))))
+
+    def em_estimator(self, X):
+
+        log_llh = 0.0
+
+        log_llh = self.calc_log_likelihood(X)
+        gama_Znk = self.calc_gama_Znk(X)
+        Nk = np.sum(gama_Znk, axis=0)
+        N  = gama_Znk.shape[0]
+
+        mu_new = self.update_mu(X, gama_Znk, Nk)
+        sigma_new = self.update_sigma(X, gama_Znk, Nk, mu_new)
+        pi_new = Nk / N
+
+        self.mu = mu_new
+        self.sigma = sigma_new
+        self.pi = pi_new
+
+        return log_llh
+
+def train(gmms, num_iterations = num_iterations):
+    dict_utt2feat, dict_target2utt = read_feats_and_targets(
+            'train/feats.scp', 'train/text')
+    
+    for target in targets:
+        feats = get_feats(target, dict_utt2feat, dict_target2utt)   #
+        for i in range(num_iterations):
+            log_llh = gmms[target].em_estimator(feats)
+            print("Target:{} iteration:{} likelyhood:{}".format(
+                target, i, log_llh))
+    return gmms
+
+def test(gmms):
+    correction_num = 0
+    error_num = 0
+    acc = 0.0
+    dict_utt2feat, dict_target2utt = read_feats_and_targets('test/feats.scp', 'test/text')
+    dict_utt2target = {}
+    for target in targets:
+        utts = dict_target2utt[target]
+        for utt in utts:
+            dict_utt2target[utt] = target
+    for utt in dict_utt2feat.keys():
+        feats = kaldi_io.read_mat(dict_utt2feat[utt])
+        scores = []
+        for target in targets:
+            scores.append(gmms[target].calc_log_likelihood(feats))
+        predict_target = targets[scores.index(max(scores))]
+        if predict_target == dict_utt2target[utt]:
+            correction_num += 1
+        else:
+            error_num += 1
+    acc = correction_num * 1.0 / (correction_num + error_num)
+    return acc
 
 def main():
     gmms = {}
     for target in targets:
-        print(target)
-        
+        gmms[target] = GMM(39, num_gaussian, 'train/feats.scp')
+    gmms = train(gmms)
+    acc = test(gmms)
+    print('Recognition accuracy: %f' % acc)
+    fid = open('acc.txt', 'w')
+    fid.write(str(acc))
+    fid.close()
+
 if __name__=="__main__":
     main()
