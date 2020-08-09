f760682c5195083c0ceaf7ed162881cae4498204
Ubuntu <zhb502@MySSH.mewh1t20ihnehng0cyxfpntk5a.ix.internal.cloudapp.net>
2020-08-09 10:03:26 +0000

    start

---
+++ 03-GMM-EM/my_gmm.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+from utils import *
+import scipy.cluster.vq as vq
+
+num_gaussian = 5
+num_uterations = 5
+targets = ['Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'O']
+
+
+def main():
+    gmms = {}
+    for target in targets:
+        print(target)
+        
+if __name__=="__main__":
+    main()
